e3e58d6f8523a3f3931a2be1d897195c0970f628
RaphaelleC <raphaelle.charrier@gmail.com>
2021-07-02 11:51:48 +0100

    prep for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,9 @@
 export const dbURL = 'mongodb://localhost/activitiesdb'
 
-export const secret = 'horsebatterystapleredballoon'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI =
+  process.env.DB_URI || 'mongodb://localhost/activitiesdb'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
